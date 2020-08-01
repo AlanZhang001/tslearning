fa2974d9fbb436c1bb0dae6e6393074935239a66
alanzhang001 <alanzhang001@qq.com>
2020-08-01 16:57:44 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: 'module',
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+        },
+    },
+    extends: 'futu',
+    rules:{
+        "semi": ["error", "always"],
+        "no-console": 0,
+        "no-shadow": ["error", { "builtinGlobals": false, "hoist": "functions", "allow": [] }]
+    },
+    globals:{
+    },
+    env: {
+        "es6": true,
+        "node": true
+    }
+};
