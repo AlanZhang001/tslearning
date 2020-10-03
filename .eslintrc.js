baf9e563a6b8ba27b42ff239bbe2e5701d1f1dc3
alanzhang001 <alanzhang001@qq.com>
2020-10-03 16:52:30 +0800

    update readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,14 @@ module.exports = {
     plugins:['@typescript-eslint'],
     rules:{
         "semi": ["error", "always"],
-        "no-console": 0
+        "no-console": 0,
+         // 禁止使用 var
+         'no-var': "error",
+         // 优先使用 interface 而不是 type
+         '@typescript-eslint/consistent-type-definitions': [
+             "error",
+             "interface"
+         ]
     },
     globals:{
     },
